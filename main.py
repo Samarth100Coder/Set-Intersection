eeb8249d0038619b598307012209b0b316f370ed
Samarth100Coder <kumarsamarth272@gmail.com>
2024-10-21 20:28:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a={5,7,89,98,110}
+print('Set a:',a)
+b={8,110,98,65,7,12}
+print('set b:',b)
+print('Intesection:',a.intersection(b))
+print('Union:',a.union(b))
+print('Difference:',a.difference(b))
+print('Symmetric Difference:',a.symmetric_difference(b))
